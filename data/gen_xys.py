ad4cd682fd7beb4af22491439f4b0f2138eac9dd
Chen <yangcnju@gmail.com>
2017-07-09 10:18:03 -0400

    random_line range is allowed to move

--- data/gen_xys.py
+++ data/gen_xys.py
@@ -4,10 +4,10 @@ from random_line import random_line
 
 # generate points from two lines
 points = []
-xs, ys = random_line(2,2.9,0.15,size=40)
+xs, ys = random_line(2,2.9,0.15,size=40,start=-1,end=1)
 for x,y in zip(xs,ys):
     points.append((x,y,1.0))
-xs, ys = random_line(1,3.0,0.15,size=40)
+xs, ys = random_line(1,3.0,0.15,size=40,start=-1.1,end=1)
 for x,y in zip(xs,ys):
     points.append((x,y,1.0))
 
@@ -16,7 +16,7 @@ with open('xys_2lines.txt','w') as f:
         x,y,w = p
         f.write(str(x) + "," + str(y)  + "," + str(w) + "\n")
 
-xs, ys = random_line(-0.1,3.1,0.1,size=40)
+xs, ys = random_line(-0.1,3.1,0.1,size=40,start=-0.9,end=1.05)
 for x,y in zip(xs,ys):
     points.append((x,y,1.0))
 
--- data/random_line.py
+++ data/random_line.py
@@ -3,10 +3,10 @@
 import numpy as np
 import scipy.stats
 
-def random_line(a, b, sigma, size=10):
+def random_line(a, b, sigma, size=10,start=-1,end=1):
     """random_line() generate random points arround y=a*x+b,
     with normal distribution defined using sigma"""
-    xdata = np.linspace(-1,1,size)
+    xdata = np.linspace(start,end,size)
     errors = scipy.stats.norm.rvs(loc=0,scale=sigma, size=size)
     ydata = a*xdata + b + errors
     return xdata, ydata
